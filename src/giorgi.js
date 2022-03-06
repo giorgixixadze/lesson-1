d290657e4c4197d5731b073235765e4796a47c40
giorgixixadze <94007888+giorgixixadze@users.noreply.github.com>
2022-03-06 17:40:38 +0400

    gio

---
+++ src/giorgi.js
@@ -0,0 +1,10 @@
+const gio = ({user,age})=>{
+    return(
+        <div>
+            
+            <h3>welcome,{user}</h3>
+            <h4>your age {age}</h4>
+        </div>
+    )
+}
+export default gio;
